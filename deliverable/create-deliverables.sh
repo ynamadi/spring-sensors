40494b21e0c4c2fa3f89b3076f34ef5e69834f43
Yunusa M. Namadi <98776848+ynamadi@users.noreply.github.com>
2022-08-11 02:15:13 -0400

    Deliverables

--- deliverable/create-deliverables.sh
+++ deliverable/create-deliverables.sh
@@ -2,7 +2,7 @@ kubectl config use-context tap-build-cluster-admin@tap-build-cluster
 
 kubectl get deliverable spring-sensors --namespace tap-dev -o json > deliverable-temp.json
 jq 'del(.status)' deliverable-temp.json | jq 'del(.metadata.ownerReferences)' > deliverable.json && rm -rf deliverable-temp.json
-jq '.spec.source.git.ref.branch |= "develop"' deliverable.json > deliverable-staging.json
+jq '.spec.source.git.ref.branch |= "main"' deliverable.json > deliverable-staging.json
 jq '.spec.source.git.ref.branch |= "main"' deliverable.json > deliverable-live.json
 rm -rf deliverable.json
 
@@ -13,3 +13,5 @@ kubectl apply -f deliverable-staging.json
 # Apply deliverable to prod/live cluster (This points to main branch)
 kubectl config use-context tap-run-prod-cluster-admin@tap-run-prod-cluster
 kubectl apply -f deliverable-live.json
+
+kubectl config use-context tap-build-cluster-admin@tap-build-cluster
